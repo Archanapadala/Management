8258d049af1f54f0885499d122b0e07cd0219677
unknown <ninni2796@gmail.com>
2024-12-05 18:52:43 -0600

    entities

---
+++ Core/Entities/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualBasic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Core.Entities
+{
+    public abstract class BaseEntity
+    {
+        [Key]
+        public long Id { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+             public DateTime UpdatedAt { get; set; } = DateTime.Now;
+        public bool IsActive { get; set; } = true;
+
+
+    }
+}
